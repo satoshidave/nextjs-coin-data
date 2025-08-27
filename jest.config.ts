eb816ba40a802b119ba48f7695fd00fa0c95fc60
David Porras <david.porras.amaris@esmax.cl>
2025-08-27 17:23:56 -0400

    ✨ Config files & README.md

--- jest.config.ts
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	testEnvironment: "jsdom",
-	transform: {
-		"^.+\\.(ts|tsx)$": "babel-jest",
-	},
-	moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-	setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-};
